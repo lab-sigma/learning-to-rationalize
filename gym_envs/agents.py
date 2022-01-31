3805d3d05306cf6afd005fc64fa8702c6f9284b9
Param Damle <paramsdamle@gmail.com>
2022-01-30 23:36:47 -0500

    Added Gym version of MWU-MB

--- gym_envs/agents.py
+++ gym_envs/agents.py
@@ -27,3 +27,32 @@ class EXP3DH:
 		exp_loss = np.exp( self.loss - np.max(self.loss) )
 
 		self.action_prob =  (1-self.eps) * exp_loss / np.sum(exp_loss) + self.eps / self.num_actions
+
+
+# Mean-based multiplicative weight update from Deng, et al
+class MWUMB:
+	def __init__(self, num_actions, num_iterations=None, beta=None, b=0.2):
+		self.num_actions = num_actions
+		### not essential, but use higher precision just in case
+		self.weights = np.ones(num_actions, dtype=np.longdouble)
+		self.total_weight = num_actions
+		self.total_reward = np.zeros(num_actions, dtype=np.longdouble)
+		self.eps = 1  # epsilon t, parameter that's decreasing with time. O(1/sqt(t))
+		self.action_prob = np.ones(num_actions, dtype=np.longdouble) / num_actions
+		self.t = 0
+
+	def __str__(self):
+		return f"MWU-MB\naction_prob={self.action_prob}\n"
+
+	def feedback(self, action, reward, state=None):
+		self.t += 1
+
+		self.eps = 1 / ((self.t + 1) ** .5)
+		estimatedReward = reward / self.action_prob[action]
+
+		# Deng, et al
+		self.total_reward[action] += estimatedReward
+		self.weights = np.exp(self.eps * self.total_reward)
+		self.total_weight = np.sum(self.weights)
+
+		self.action_prob = ((1.0 - self.eps) * self.weights / self.total_weight) + (self.eps / self.num_actions)
